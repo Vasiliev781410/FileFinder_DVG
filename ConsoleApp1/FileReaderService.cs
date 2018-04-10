fa20418f04a789eeb2fff80ae24e548a82f40d71
Pasha Dedukh <castiel.galante@gmail.com>
2018-04-10 21:47:36 +0300

    skelet

---
+++ ConsoleApp1/FileReaderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwo
+{
+    public interface IFileReaderService
+    {
+        string GetFileContent(string location, string fileName);
+    }
+
+    public class FileReaderService : IFileReaderService
+    {
+        public string GetFileContent(string location, string fileName)
+        {
+            //your code here
+        }
+    }
+}
+
---
+++ ConsoleApp1/FinderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwo
+{
+    public interface IFinderService
+    {
+        string FindFile(string locationToSearch, string fileName);
+    }
+
+    public class FinderService : IFinderService
+    {
+        public string FindFile(string locationToSearch, string fileName)
+        {
+            //your code here
+            //ss
+            //zz
+            //aa
+            //qq
+        }
+    }
+}
