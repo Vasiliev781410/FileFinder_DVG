411fccbdac7d76642f37d180f78079d49dabf0ad
Sergei <vasilyev781410@gmail.com>
2018-04-10 20:42:52 +0300

    base

---
+++ ConsoleApp1/FileSaverService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwo
+{
+    public interface IFileSaverService
+    {
+        bool SaveFile(string content, string lcoation, string fileName);
+    }
+
+    public class FileSaverService : IFileSaverService
+    {
+        public bool SaveFile(string content, string lcoation, string fileName)
+        {
+            //Your code here
+        }
+    }
+}
+
