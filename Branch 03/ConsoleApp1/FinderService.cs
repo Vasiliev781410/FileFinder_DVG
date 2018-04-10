4f7222675e4a19d9ca9e3959066c167f618b34b1
Sergei <vasilyev781410@gmail.com>
2018-04-10 22:33:42 +0300

    test

---
+++ Branch 03/ConsoleApp1/FinderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleTwo
+{
+    public interface IFinderService
+    {
+        string FindFile(string locationToSearch, string fileName);
+    }
+
+    public class FinderService : IFinderService
+    {
+        public string FindFile(string locationToSearch, string fileName)
+        {
+            //your code here
+        }
+    }
+}
